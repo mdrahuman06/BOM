771487045d5df98981559cc971e72f5917c9544a
mdrahuman06 <ragu20140610@gmail.com>
2025-04-29 14:59:19 +0530

    files added

--- bom_app/models.py
+++ bom_app/models.py
@@ -22,7 +22,7 @@ class BOMItem(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='bom_items')
     item_name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=0, default=0.0)
     item_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=10, choices=UNIT_CHOICES)
